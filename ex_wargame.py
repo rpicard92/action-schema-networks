08ca2c88433cbaf0585590a3c5dca43cc3bcc6c6
ron <rpicard92@gmail.com>
2019-07-22 08:43:19 -0500

    initial results

---
+++ ex_wargame.py
@@ -0,0 +1,13 @@
+"""For experiments on Exploding Blocksworld."""
+
+PDDL_DIR = '../problems/ippc08/domains/wargame/'
+COMMON_PDDLS = ['domain.pddl']
+TRAIN_PDDLS = [
+    'wargame_5_p01.pddl'
+]
+TRAIN_NAMES = None
+TEST_RUNS = [
+    (['wargame_5_p01.pddl'], None),
+    (['wargame_5_p02.pddl'], None)
+
+]  # yapf: disable
